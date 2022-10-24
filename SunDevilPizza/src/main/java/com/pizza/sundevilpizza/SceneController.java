6def811e28ba49c0c56089948ff22e3b16488e88
Munachimso Aneke <52995219+anekemuna@users.noreply.github.com>
2022-10-23 19:23:27 -0700

    Linked Buttons
    Linked Buttons in intial_customer_page and startup_page

--- SunDevilPizza/src/main/java/com/pizza/sundevilpizza/SceneController.java
+++ SunDevilPizza/src/main/java/com/pizza/sundevilpizza/SceneController.java
@@ -1,29 +1,19 @@
 package com.pizza.sundevilpizza;
 
 import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloController {
-  /*  @FXML
-    private Label welcomeText;
-
-    @FXML
-   protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
-*/
+public class SceneController {
     private Stage stage;
     private Scene scene;
 
-    public void switchToChoosePizza(ActionEvent event) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(SunDevilPizzaApplication.class.getResource("choose_pizza_page.fxml"));
+    public void switchToCustomerPage(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(SunDevilPizzaApplication.class.getResource("initial_customer_page.fxml"));
         stage = (Stage)((Node)event.getSource()).getScene().getWindow();
         scene = new Scene(fxmlLoader.load(), 900, 600);
         stage.setTitle("SunDevil Pizza");
