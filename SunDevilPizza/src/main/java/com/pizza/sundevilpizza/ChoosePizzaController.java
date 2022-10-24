a83341d72d436c0ea45053b25ed854619bf28af5
Munachimso Aneke <52995219+anekemuna@users.noreply.github.com>
2022-10-23 19:52:53 -0700

    Created ChoosePizza Controller
    Linked Pizza to Toppings

--- SunDevilPizza/src/main/java/com/pizza/sundevilpizza/ChoosePizzaController.java
+++ SunDevilPizza/src/main/java/com/pizza/sundevilpizza/ChoosePizzaController.java
@@ -1,29 +1,21 @@
 package com.pizza.sundevilpizza;
 
 import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloController {
-  /*  @FXML
-    private Label welcomeText;
+public class ChoosePizzaController {
 
-    @FXML
-   protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
-*/
     private Stage stage;
     private Scene scene;
 
-    public void switchToChoosePizza(ActionEvent event) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(SunDevilPizzaApplication.class.getResource("choose_pizza_page.fxml"));
+    // Link any pizza Button to Next Scene
+    public void switchToToppingPage(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(SunDevilPizzaApplication.class.getResource("choose_toppings_page.fxml"));
         stage = (Stage)((Node)event.getSource()).getScene().getWindow();
         scene = new Scene(fxmlLoader.load(), 900, 600);
         stage.setTitle("SunDevil Pizza");
--- SunDevilPizza/src/main/java/com/pizza/sundevilpizza/InitialCustomerController.java
+++ SunDevilPizza/src/main/java/com/pizza/sundevilpizza/InitialCustomerController.java
@@ -8,12 +8,13 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class SceneController {
+public class InitialCustomerController {
     private Stage stage;
     private Scene scene;
 
-    public void switchToCustomerPage(ActionEvent event) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(SunDevilPizzaApplication.class.getResource("initial_customer_page.fxml"));
+    // Link Place Order Button to Next Scene
+    public void switchToChoosePizza(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(SunDevilPizzaApplication.class.getResource("choose_pizza_page.fxml"));
         stage = (Stage)((Node)event.getSource()).getScene().getWindow();
         scene = new Scene(fxmlLoader.load(), 900, 600);
         stage.setTitle("SunDevil Pizza");
