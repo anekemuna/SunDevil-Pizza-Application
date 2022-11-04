848329eb455930294e5d38d440fa3f95babca5c8
yliu887 <81446401+yliu887@users.noreply.github.com>
2022-11-04 14:03:50 -0700

    Add files via upload

---
+++ SunDevilPizza/src/main/java/OrderList.java
@@ -0,0 +1,79 @@
+//by YungLing Liu
+package Functions;
+import java.util.ArrayList;
+import Functions.Order;
+import Functions.Pizza;
+
+public class OrderList {
+    private static ArrayList<Order> orderList;
+
+    /**
+     * Constructor to instantiate orderList
+     */
+    public OrderList(){
+        orderList = new ArrayList<Order>();
+    }
+
+    /**
+     * to add a new order to the orderList
+     * @param newOrder
+     */
+    public void addOrder(Order newOrder){
+        orderList.add(newOrder);
+    }
+
+    /**
+     * find the order by name
+     * it will return the index of the order
+     * if order not found, it will return -1
+     * @param inputName
+     * @return
+     */
+    public int findOrder(String inputName){
+        for(int i = 0; i < orderList.size(); i++){
+            String current = orderList.get(i).getName();
+            if (current.equals(inputName)){
+                return i;
+            }
+        }
+        System.out.println("Order not found");
+        return -1;
+    }
+
+    /**
+     * delete an order from orderList by name
+     * @param inputName
+     */
+    public void deleteOrder(String inputName){
+        int index = findOrder(inputName);
+        if (index != -1){
+            orderList.remove(index);
+        }
+    }
+
+    /**
+     * delete an order from orderList by index
+     * @param index
+     */
+    public void deleteOrder(int index){
+        orderList.remove(index);
+    }
+
+    /**
+     * get the size of the orderList
+     * @return
+     */
+    public int getSize(){
+        return orderList.size();
+    }
+
+    /**
+     * print the whole orderList
+     */
+    public void printOrderList(){
+        for(int i = 0; i < orderList.size(); i++){
+            orderList.get(i).printOrder();
+            System.out.println(" ");
+        }
+    }
+}
---
+++ SunDevilPizza/src/main/java/Pizza.java
@@ -0,0 +1,81 @@
+//by YungLing Liu
+package Functions;
+import java.util.ArrayList;
+
+public class Pizza {
+    private String type;
+    private double price;
+    private static ArrayList<String> toppings;
+
+    /**
+     * Constructor
+     *  default will instantiate everything as null
+     *  and toppings list (String) will be instantiated
+     */
+    public Pizza(){
+        type = null;
+        price = 0;
+        toppings = new ArrayList<String>();
+    }
+
+    /**
+     * setter function for type
+     * @param orderType
+     */
+    public void setType(String orderType){
+        type = orderType;
+    }
+
+    /**
+     * setter function for price
+     * @param inputPrice
+     */
+    public void setPrice(double inputPrice){
+        price = inputPrice;
+    }
+
+    /**
+     * getter function for type
+     * @return
+     */
+    public String getType(){
+        return type;
+    }
+
+    /**
+     * getter function for price
+     * @return
+     */
+    public double getPrice(){
+        return price;
+    }
+
+    /**
+     * to add topping to the topping list
+     * Topping options are: "Mushroom", "Onion", "Olives", "Extra cheese"
+     * @param inputTopping
+     */
+    public void addToppings(String inputTopping){
+        toppings.add(inputTopping);
+    }
+
+    /**
+     * print out the details of the pizza
+     * output format
+     * Pizza type: type
+     * Pizza price: price
+     * Pizza toppings:
+     * topping1
+     * topping2
+     * ...
+     */
+    public void printPizza(){
+        System.out.println("Pizza type: " + type);
+        System.out.println("Pizza price: " + price);
+        System.out.println("Pizza toppings: ");
+        for(int i = 0; i < toppings.size(); i++){
+            System.out.println(toppings.get(i));
+        }
+    }
+
+}
