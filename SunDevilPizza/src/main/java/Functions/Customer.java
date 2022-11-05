4ae4df694f0f4e80981ff71a0ddc1b65c0e35da0
DikshitaB <dbuzarba@asu.edu>
2022-11-04 21:25:50 -0700

    Updated customer class

--- SunDevilPizza/src/main/java/Functions/Customer.java
+++ SunDevilPizza/src/main/java/Functions/Customer.java
@@ -4,5 +4,35 @@
 package Functions;
 
 public class Customer extends SundevilPizza {
+    private String name;
+    private String ASUID;
+    private Pizza pizzaType;
+    private String[] toppings;
+    private Order order;
+    public String status;
 
+    public void choosePizza(String type){
+        this.type = type;
+    }
+    public void chooseToppings(){
+
+    }
+    public void setName(String inputName){
+        name = inputName;
+    }
+    public String getName(){
+        return name;
+    }
+    public void setPizza(Pizza inputPizza){
+        pizzaType = inputPizza;
+    }
+    public Pizza getPizza(){
+        return pizzaType;
+    }
+    public void setStatus(String inputStatus){
+        status = inputStatus;
+    }
+    public String getStatus(){
+        return status;
+    }
 }
