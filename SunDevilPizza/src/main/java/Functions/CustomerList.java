995afc71bbbaa71adb68995f9ca357fa79b05458
DikshitaB <dbuzarba@asu.edu>
2022-11-11 21:31:53 -0700

    CustomerList

---
+++ SunDevilPizza/src/main/java/Functions/CustomerList.java
@@ -0,0 +1,19 @@
+package Functions;
+
+import java.util.ArrayList;
+
+public class CustomerList {
+    private ArrayList<Customer> customerList;
+
+    public CustomerList(){
+
+        customerList = new ArrayList<Customer>();
+    }
+    public void addCustomer(Customer newCustomer){
+
+        customerList.add(newCustomer);
+    }
+    public Customer getCustomer(int index){
+        return customerList.get(index);
+    }
+}
