5dad71426117ea0523d5909b69b9756c022e9eae
romeobarrientos <rgbarrie@asu.edu>
2022-11-13 19:01:58 -0700

    Extend processing agent, changed from TableView to Listview

--- SunDevilPizza/src/main/java/com/pizza/sundevilpizza/processingAgentController.java
+++ SunDevilPizza/src/main/java/com/pizza/sundevilpizza/processingAgentController.java
@@ -1,19 +1,29 @@
 package com.pizza.sundevilpizza;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 import javafx.scene.control.Button;
 import javafx.scene.control.*;
 import Functions.*;
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
 import Functions.Staff;
 
-public class processingAgentController extends Staff {
+public class processingAgentController extends ProcessingAgent {
     private Stage stage;
 
     private Scene scene;
@@ -24,21 +34,17 @@ public class processingAgentController extends Staff {
     @FXML
     private Button back;
 
-    // Implementing table population using Order class
     @FXML
-    private TableView<Order> table;
+    private ListView processList;
 
-    @FXML
-    private TableView<Order> name;
-
-    @FXML
-    private TableView<Order> pizza;
 
 
     public void chefReady(ActionEvent event) throws IOException {
-     //   changeStatus(name, "Ready to Cook");
-     //   sendToChef(Order order);
+           changeStatus(name, "Cooking");
+           sendToChef(Order order);
     }
+
+
 
     public void toWelcomePage(ActionEvent event) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(SunDevilPizzaApplication.class.getResource("startup_page.fxml"));
@@ -47,5 +53,19 @@ public class processingAgentController extends Staff {
         stage.setTitle("SunDevil Pizza");
         stage.setScene(scene);
         stage.show();
+    }
+
+    public void customerOrder(){
+
+        for(int i = 0; i < list.getSize(); i++) {
+        Order current = new Order();
+        current = list.getOrder(i);
+        String orderName = current.getName();
+        String pizzaType = current.getPizza().getType();
+        String pizzaToppings = current.getPizza().getToppings();
+        Pane pane = new Pane();
+        pane = processPane(orderName, pizzaType, pizzaToppings);
+        processList.getItems().add(pane);
+    }
     }
 }
