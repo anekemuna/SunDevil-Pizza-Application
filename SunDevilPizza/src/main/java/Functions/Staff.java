eeed836d90699aa355d690eafa722b2316f6dd00
yliu887 <81446401+yliu887@users.noreply.github.com>
2022-11-18 13:05:23 -0700

    Add files via upload

---
+++ SunDevilPizza/src/main/java/Functions/Staff.java
@@ -0,0 +1,27 @@
+//by YungLing Liu
+/**
+ * This is the child class of SundevilPizza and the parent class of ProcessingAgent and Chef
+ * This class contains a list of orders called "listForChef", which is the list for the chef.
+ * This list is accessible for the child classes (ProcessingAgent and Chef)
+ * (Order should only be in this list once it's sent to the chef by the processing agent) <- This function is implemented in ProcessingAgent
+ * This class also contains functions that will be useful for both ProcessingAgent and Chef (i.e. changeStatus)
+ */
+package Functions;
+import Functions.OrderList;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public abstract class Staff extends SundevilPizza {
+    /**
+     * This function let staff (both Processing Agent and Chef) change the status of the order
+     * parae
+     *
+     * @param name
+     * @param status
+     */
+    protected void changeStatus(String name, String status) {
+        int index = list.findOrder(name);
+        list.getOrder(index).setStatus(status);
+    }
+}
